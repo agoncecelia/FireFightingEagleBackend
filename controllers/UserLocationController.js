aecb3599cdcd1cefba7c20262db3c7c27ed82059
agoncecelia <agonn.c@gmail.com>
2017-05-19 00:49:39 +0200

    fixed updateLocation

--- controllers/UserLocationController.js
+++ controllers/UserLocationController.js
@@ -3,21 +3,18 @@ var UserLocation = require('../models/userLocation');
 module.exports = {
     updateLocation: function(req, res) {
         var body = req.body;
-
         UserLocation.findOne({"imei": body.imei}, function(err, user){
             if(user != null){
                 user.location.coordinates = [body.location.coordinates[0], body.location.coordinates[1]];
                 user.gcmToken = body.gcmToken;
-
                 user.save();
                 res.send({
                     msg: "user location updated succesfully"
                 });
-                
             }else{
                var newUserLocation = new UserLocation({
                     gcmToken: body.gcmToken,
-                    imei: body.imei,
+                    imei: body.deviceIMEI,
                     location: {
                         type: "Point",
                         coordinates: [body.location.coordinates[0], body.location.coordinates[1]]
